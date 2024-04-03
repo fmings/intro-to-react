6fb69c2d25930816997ba5a7bd011844af1fe109
fmings <mingsfelicia@gmail.com>
2024-04-02 19:55:56 -0500

    basic counter app coding

---
+++ components/Counter.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export default function Counter({ title }) {
+  const [value, setValue] = useState(0);
+
+  const handleClick = (action) => {
+    switch (action) {
+      case 'Increment':
+        setValue((prevState) => prevState + 1);
+        break;
+      case 'Decrement':
+        setValue((prevState) => prevState - 1);
+        break;
+      case 'Reset':
+        setValue(0);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <h2>{value}</h2>
+      <button type="button" onClick={() => handleClick('Increment')}>Increment</button>
+      {value <= 0 ? '' : <button type="button" onClick={() => handleClick('Decrement')}>Decrement</button>}
+      <button type="button" onClick={() => handleClick('Reset')}>Reset</button>
+    </>
+  );
+}
+
+Counter.propTypes = {
+  title: PropTypes.string,
+};
+
+Counter.defaultProps = {
+  title: 'Title of the Coutner',
+};
